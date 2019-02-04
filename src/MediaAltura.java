3aa45818fd0d7d01344eca9ec6409b3271828638
RobertoGarciaManchon <robertogm3@gmail.com>
2019-02-04 03:08:48 +0100

    añadidos archivos2

---
+++ src/MediaAltura.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class MediaAltura {
+
+	public static void main(String[] args) {
+		double[] altura = new double[10];
+		double media = 0, suma = 0, mayorAltura=0, menorAltura=3;
+		Scanner S = new Scanner(System.in);
+
+		for (int i = 0; i < 10; i++) {
+			System.out.println("Introduzca una altura");
+			altura[i] = S.nextDouble();
+			suma = suma + altura[i];
+		}
+		media = suma / 10;
+
+		for (int i = 0; i < 10; i++) {
+			if (altura[i] > mayorAltura) {
+				mayorAltura = altura[i];
+			}
+			if (altura[i] < menorAltura) {
+				menorAltura = altura[i];
+			}
+		}
+		System.out.println(media);
+		System.out.println(mayorAltura);
+		System.out.println(menorAltura);
+	}
+
+}
